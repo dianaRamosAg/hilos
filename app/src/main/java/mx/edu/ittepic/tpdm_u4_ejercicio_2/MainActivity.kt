c5476bcb63958ef4bb2b9d62cc4e6b5289151499
dianaRamosAg <dilaramosag@ittepic.edu.mx>
2019-11-13 21:34:42 -0600

    first commit

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u4_ejercicio_2/MainActivity.kt
@@ -0,0 +1,90 @@
+package mx.edu.ittepic.tpdm_u4_ejercicio_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var resultado:EditText?=null
+    var bciclo:Button?=null
+    var bhilo:Button?=null
+    var btimer:Button?=null
+    var etiqueta:TextView?=null
+    var ytint=0
+    var vhint=0
+    private var hilo=Hilo(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultado=findViewById(R.id.resultado)
+        bhilo=findViewById(R.id.hilo)
+        btimer=findViewById(R.id.timer)
+        bciclo=findViewById(R.id.ciclo)
+        etiqueta=findViewById(R.id.etiqueta)
+
+
+        bciclo?.setOnClickListener {
+            hilo.stop()
+        /*    (0..100000).forEach{
+                etiqueta?.setText("Valor :"+it)
+            }*/
+        }
+
+        btimer?.setOnClickListener{
+    /*
+            var timer=object:CountDownTimer(2000,20)
+            {
+                override fun onFinish() {
+                    start()
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+                    etiqueta?.setText("Valor :"+ytint++)
+                }
+
+            }.start()*/
+            hilo.resume()
+        }
+
+        bhilo?.setOnClickListener{
+
+            hilo.start()
+          //  hilo.resume()
+                }
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+private class Hilo(p:MainActivity):Thread(){
+
+    var puntero=p
+
+    override fun run() {
+        super.run()
+        while(true) {
+            sleep(200)
+            puntero.runOnUiThread {
+                //aqui se puede ejecutar para el mainactivity
+                puntero.setTitle("Valor : "+puntero.vhint++)
+            }
+
+        }
+
+    }
+
+
+
+}
